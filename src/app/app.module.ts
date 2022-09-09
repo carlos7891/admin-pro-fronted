a8fc5eb070be968920bc41d304e0adaae79b5906
carlos.espinosa@segurosbolivar.com-desarrollador <carlos.espinosa@segurosbolivar.com>
2022-09-09 09:26:56 -0500

    Se realizan las primeras pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NopagesfoundComponent } from './pages/nopagesfound/nopagesfound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graph1Component } from './pages/graph1/graph1.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NopagesfoundComponent,
+    DashboardComponent,
+    SidebarComponent,
+    HeaderComponent,
+    BreadcrumbsComponent,
+    ProgressComponent,
+    Graph1Component,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
